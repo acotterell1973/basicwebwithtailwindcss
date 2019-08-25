e1eefe01ae7701839885c753ccca40a9f235e3d0
Twisted Cloud Solutions <developer@twistedcloudsolutions.com>
2019-08-25 15:40:26 -0400

    basic website setup .net core 3.0

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+'use strict';
+
+// Global Configs ============================================================
+const isDev = ((process.env.NODE_ENV || 'development').trim().toLowerCase() === 'development');
+const isProd = !isDev;
+const paths = {
+    root: './',
+    src: './src/',
+    build: './wwwroot/'
+};
+
+const libsBasePath = './node_modules/';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+var cssConfig = {
+    src: paths.src + '/scss/*.*/*.scss',
+    dest: paths.build + 'assets/css'
+};
+
+gulp.task('convert-sass', function () {
+    return gulp.src(cssConfig.src)
+        .pipe(sass())
+        .pipe(cssConfig.dest);
+});
+
+gulp.task('watch', function () {
+    //css changes
+    gulp.watch(cssConfig.src, ['convert-sass'] );
+});
+
